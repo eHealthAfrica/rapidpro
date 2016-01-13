07911e77b78af5e86ede98afcef5f325161c4435
nkhumphreys <nkhumphreys@gmail.com>
2016-01-13 17:35:14 +0100

    Correct #! and add branch condition

--- travis_success.sh
+++ travis_success.sh
@@ -1,13 +1,16 @@
-#1/bin/bash
+#!/bin/bash
 set -e
 
 TAG=$(git tag | sort -r | tail -1)
+BRANCH=$(git branch | grep "^*" | cut -d" " -f2)
 
 if [ -z "$TAG" ]; then
     echo "No tags, aborting"
     exit -1
 else
-    aws ecr get-login --region=us-east-1 | bash
-    docker tag rapidpro_rapidpro:latest 387526361725.dkr.ecr.us-east-1.amazonaws.com/rapidpro:$TAG
-    docker push 387526361725.dkr.ecr.us-east-1.amazonaws.com/rapidpro:$TAG
+    if [ "$BRANCH" -eq "feature/nathan/dockerize"]; then
+        aws ecr get-login --region=us-east-1 | bash
+        docker tag rapidpro_rapidpro:latest 387526361725.dkr.ecr.us-east-1.amazonaws.com/rapidpro:$TAG
+        docker push 387526361725.dkr.ecr.us-east-1.amazonaws.com/rapidpro:$TAG
+    fi
 fi
