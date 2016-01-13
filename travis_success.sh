38bc7c807b8a357fce5be18c8353d71480188ca8
nkhumphreys <nkhumphreys@gmail.com>
2016-01-13 17:00:58 +0000

    Use string check and not integer

--- travis_success.sh
+++ travis_success.sh
@@ -8,7 +8,7 @@ if [ -z "$TAG" ]; then
     echo "No tags, aborting"
     exit -1
 else
-    if [ "$BRANCH" -eq "feature/nathan/dockerize" ]; then
+    if [ "$BRANCH" = "feature/nathan/dockerize" ]; then
         aws ecr get-login --region=us-east-1 | bash
         docker tag rapidpro_rapidpro:latest 387526361725.dkr.ecr.us-east-1.amazonaws.com/rapidpro:$TAG
         docker push 387526361725.dkr.ecr.us-east-1.amazonaws.com/rapidpro:$TAG
