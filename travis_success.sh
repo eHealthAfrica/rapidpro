7716756d93bc833b016d11dd030b800cc7ca57c9
nkhumphreys <nkhumphreys@gmail.com>
2016-01-13 23:09:21 +0100

    Try a different format to push docker image

--- travis_success.sh
+++ travis_success.sh
@@ -4,12 +4,17 @@ set -e
 TAG=$(git describe --tags $(git rev-parse HEAD))
 BRANCH=$(git branch | grep "^*" | cut -d" " -f2)
 
-if [ -z "$TAG" ]; then
+echo $TAG
+echo $BRANCH
+
+if [ -z $TAG ]
+then
     echo "No tags, tagging as: latest"
     TAG="latest"
 fi
 
-if [ "$BRANCH" = "feature/nathan/dockerize" ]; then
+if [ $BRANCH = "feature/nathan/dockerize" ]
+then
     aws ecr get-login --region=us-east-1 | bash
     docker tag rapidpro_rapidpro:latest 387526361725.dkr.ecr.us-east-1.amazonaws.com/rapidpro:$TAG
     docker push 387526361725.dkr.ecr.us-east-1.amazonaws.com/rapidpro:$TAG
