f5c9c06d655032bf60aebe62ccdafa8b8d0af9bf
nkhumphreys <nkhumphreys@gmail.com>
2016-01-13 18:07:15 +0100

    tag as latest if there is no tag

--- travis_success.sh
+++ travis_success.sh
@@ -5,12 +5,12 @@ TAG=$(git tag | sort -r | tail -1)
 BRANCH=$(git branch | grep "^*" | cut -d" " -f2)
 
 if [ -z "$TAG" ]; then
-    echo "No tags, aborting"
-    exit -1
-else
-    if [ "$BRANCH" = "feature/nathan/dockerize" ]; then
-        aws ecr get-login --region=us-east-1 | bash
-        docker tag rapidpro_rapidpro:latest 387526361725.dkr.ecr.us-east-1.amazonaws.com/rapidpro:$TAG
-        docker push 387526361725.dkr.ecr.us-east-1.amazonaws.com/rapidpro:$TAG
-    fi
+    echo "No tags, tagging as: latest"
+    TAG="latest"
+fi
+
+if [ "$BRANCH" = "feature/nathan/dockerize" ]; then
+    aws ecr get-login --region=us-east-1 | bash
+    docker tag rapidpro_rapidpro:latest 387526361725.dkr.ecr.us-east-1.amazonaws.com/rapidpro:$TAG
+    docker push 387526361725.dkr.ecr.us-east-1.amazonaws.com/rapidpro:$TAG
 fi
