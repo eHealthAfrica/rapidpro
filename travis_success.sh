35e3d62e8a673345f23456f208c049120905484c
nkhumphreys <nkhumphreys@gmail.com>
2016-01-13 17:20:51 +0100

    Remove debug exit and actually tag image

--- travis_success.sh
+++ travis_success.sh
@@ -7,8 +7,6 @@ if [ -z "$TAG" ]; then
     echo "No tags, aborting"
     exit -1
 else
-    echo "yes"
-    exit 0
     aws ecr get-login --region=us-east-1 | bash
     docker tag rapidpro_rapidpro:latest 387526361725.dkr.ecr.us-east-1.amazonaws.com/rapidpro:$TAG
     docker push 387526361725.dkr.ecr.us-east-1.amazonaws.com/rapidpro:$TAG
