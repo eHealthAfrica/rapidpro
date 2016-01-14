9ba4bda8e507ea63946c03c7bee55b50d7c7e23b
nkhumphreys <nkhumphreys@gmail.com>
2016-01-14 12:51:55 +0100

    Tag against master for releases

--- travis_success.sh
+++ travis_success.sh
@@ -14,7 +14,7 @@ then
     TAG="latest"
 fi
 
-if [ $BRANCH = "feature/nathan/dockerize" ]
+if [ $BRANCH = "master" ] # we also want to add -a $PR = "false" as we dont want to push PRs
 then
     aws ecr get-login --region=us-east-1 | bash
     docker tag -f rapidpro_rapidpro:latest 387526361725.dkr.ecr.us-east-1.amazonaws.com/rapidpro:$TAG
